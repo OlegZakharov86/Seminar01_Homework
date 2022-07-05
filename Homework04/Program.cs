48919e9b06d8a656f67df2ea7393ff85ceb6b843
OlegZakharov <varahas@mail.ru>
2022-07-05 22:52:21 +0300

    add homework04

---
+++ Homework04/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Введите целое число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int count = 1;
+        // int div = number/2;
+        while(count < number)
+        if ( count % 2 == 0 )
+        {
+            Console.WriteLine($" {count} ");
+            count = count + 1;
+        }
+        else
+        {
+            count = count + 1;
+        }
+        
+        }
+    }
+}
