38515e461a4f7568040b206e120abaaa1d84916d
OlegZakharov <varahas@mail.ru>
2022-07-05 21:43:56 +0300

    add homework01

---
+++ Homework01/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Введите целое число: ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите второе целое число: ");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+        if (number1 > number2)
+        {
+            Console.WriteLine($"Число {number1} больше {number2}");
+        }
+        else
+        {
+            Console.WriteLine($"Число {number1} меньше {number2}");
+        }
+        }
+    }
+}
