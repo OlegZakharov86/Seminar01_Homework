917f22d2508f41f5a1de41e258c2614f8ee7ee1a
OlegZakharov <varahas@mail.ru>
2022-07-05 22:09:05 +0300

    add homework03

---
+++ Homework03/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Введите целое число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int div = number/2;
+        if ( div % 2 == 0 )
+        {
+            Console.WriteLine($" {number} четное число");
+        }
+        else 
+        {
+            Console.WriteLine($"{number} нечетное число");
+        }
+        
+        }
+    }
+}
